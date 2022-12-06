c851c6309662f752a1a5ea6279bb28ba0d8de0b0
Ruben Jansen <ruben@opmaatzagen.nl>
2022-12-06 13:41:43 +0100

    day_6 shorter

--- 2022/day_6/src/main.rs
+++ 2022/day_6/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = include_str!("./input.txt");
 
@@ -9,15 +11,7 @@ fn calculate_start_of_packet(input: &str, window_size: usize) -> usize {
     let index = input
         .as_bytes()
         .windows(window_size)
-        .position(|window| {
-            let mut uniq = std::collections::HashSet::new();
-
-            window.into_iter().for_each(|x| {
-                uniq.insert(x);
-            });
-
-            return uniq.len() == window_size;
-        })
+        .position(|window| window.iter().cloned().collect::<HashSet<u8>>().len() == window_size)
         .expect("No valid position found");
 
     index + window_size
